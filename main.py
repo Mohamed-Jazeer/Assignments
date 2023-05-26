59c4ac1c1af7e16fdefe03dcf8d6b9d9ca8ea192
kurra <kurrafkurraf@gmail.com>
2023-05-26 23:18:31 +0800

    factorial coded

---
+++ main.py
@@ -0,0 +1,16 @@
+class Math:
+    def __init__(self, n):
+        self.n = n
+
+    def fact(n):
+        if n == 0:
+            return 1
+        else:
+            return n * Math.fact(n-1)
+
+
+n = 5
+factorial = Math.fact(n)
+print(factorial)
+
+
